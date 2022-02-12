a277906df6c291dee3c22b9465a517b0e3100c48
Revant Nandgaonkar <revant.one@gmail.com>
2022-02-12 13:04:36 +0530

    feat: add oauth2 auth code callback for azure ad b2c

--- README.md
+++ README.md
@@ -2,6 +2,39 @@
 
 Microsoft Integration for Frappe Framework
 
+### Usage
+
+Install as a standard frappe-bench app.
+
+Add a `Social Login Key` with your Microsoft Azure AD credentials.
+
+Example Social Login Key JSON:
+
+```json
+{
+  "enable_social_login": 1,
+  "provider_name": "Azure AD B2C",
+  "social_login_provider": "Custom",
+  "client_id": "96752e67-57d3-4c03-99d5-e57e28c30ef0",
+  "client_secret": "*************************************",
+  "base_url": "https://login.microsoftonline.com/95228b0c-0fd6-43d9-857c-95130599dd30",
+  "authorize_url": "/oauth2/v2.0/authorize",
+  "access_token_url": "/oauth2/v2.0/token",
+  "redirect_url": "http://localhost:8000/api/method/microsoft_integration.callback.azure_ad_b2c",
+  "api_endpoint": "https://graph.microsoft.com/oidc/userinfo",
+  "custom_base_url": 1,
+  "api_endpoint_args": "",
+  "auth_url_data": "{\"scope\": \"openid profile email\", \"response_type\": \"code\"}",
+  "user_id_property": "sub"
+}
+```
+
+Notes:
+  - Base URL is `https://login.microsoftonline.com/{tenant_id}`
+  - Use appropriate `client_id` and `client_secret`.
+  - Microsoft JWKS URL is configurable using `microsoft_jwks_url` key in `site_config.json`. Defaults to `https://login.microsoftonline.com/common/discovery/v2.0/keys`.
+  - Use name of the provider as `Azure AD B2C` as it will be converted to snake case `azure_ad_b2c` for callback. If you need to change the name, configure the snake case name using `azure_provider_key` key in `site_config.json`. Defaults to `azure_ad_b2c`.
+
 #### License
 
 MIT
---
+++ microsoft_integration/callback.py
@@ -0,0 +1,67 @@
+import base64
+import json
+
+import frappe
+import jwt
+import requests
+from frappe import _
+from frappe.utils.oauth import login_oauth_user
+
+
+@frappe.whitelist(allow_guest=True)
+def azure_ad_b2c(*args, **kwargs):
+    if isinstance(kwargs.get("state"), str):
+        state = base64.b64decode(kwargs.get("state"))
+        state = json.loads(state.decode("utf-8"))
+
+    if not (state and state["token"]):
+        frappe.respond_as_web_page(
+            _("Invalid Request"), _("Token is missing"), http_status_code=417
+        )
+
+    provider = frappe.get_doc(
+        "Social Login Key", frappe.get_conf().get("azure_provider_key", "azure_ad_b2c")
+    )
+    scope = json.loads(provider.auth_url_data).get("scope")
+    data = {
+        "grant_type": "authorization_code",
+        "client_id": provider.client_id,
+        "scope": scope,
+        "code": kwargs.get("code"),
+        "redirect_uri": provider.redirect_url,
+        "client_secret": provider.get_password("client_secret"),
+    }
+    token_response = requests.post(
+        url=provider.base_url + provider.access_token_url,
+        data=data,
+    )
+    token = token_response.json()
+    print(token.get("id_token"))
+    decoded_id_token = get_decoded_token(token.get("id_token"), provider.client_id)
+    decoded_id_token["email"] = decoded_id_token.get("preferred_username")
+    login_oauth_user(decoded_id_token, provider=provider.name, state=state)
+
+
+def get_decoded_token(token, audience):
+    r = requests.get(
+        frappe.get_conf().get(
+            "microsoft_jwks_url",
+            "https://login.microsoftonline.com/common/discovery/v2.0/keys",
+        ),
+    )
+    jwks_keys = r.json()
+    keys = jwks_keys.get("keys")
+    public_keys = {}
+    for jwk in keys:
+        kid = jwk["kid"]
+        public_keys[kid] = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+
+    kid = jwt.get_unverified_header(token)["kid"]
+    key = public_keys[kid]
+
+    return jwt.decode(
+        token + "=" * (-len(token) % 4), # padded base64 string
+        key=key,
+        algorithms=["RS256"],
+        audience=audience,
+    )
